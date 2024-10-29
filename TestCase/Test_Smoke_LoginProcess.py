7c2b886d6e024d4d824744b7dd5809a74d880ca2
Qasim201918 <164733325+Qasim201918@users.noreply.github.com>
2024-10-29 13:05:29 +0500

    File Updated

--- TestCase/Test_Smoke_LoginProcess.py
+++ TestCase/Test_Smoke_LoginProcess.py
@@ -33,9 +33,12 @@ class Test_Smoke_Login_Process():
                 assert True
             else:
                 self.driver.save_screenshot(r'.\\ScreenShot\\FailTests\\Test Case 1- Fail.png')
-                self.logger.info("************ Test Case 1 Failed: Login Failed: Unable to navigate to the Dashboard page after triggering a POST request. The backend returns a '500 Internal Server Error' status code ************")
+                self.logger.error("************ Test Case 1 Failed: Login Failed: Unable to navigate to the Dashboard page after triggering a POST request. The backend returns a '500 Internal Server Error' status code ************")
                 assert True
         except(NoSuchElementException):
             self.driver.save_screenshot(r'.\\ScreenShot\\FailTests\\Test Case 1- Fail.png')
-            self.logger.info("************ Test Case 1 Failed: Login Failed: Unable to navigate to the Dashboard page after triggering a POST request. The backend returns a '500 Internal Server Error' status code ************")
+            self.logger.error("************ Test Case 1 Failed: Login Failed: Unable to navigate to the Dashboard page after triggering a POST request. The backend returns a '500 Internal Server Error' status code ************")
             assert False, "Test Case 1- Fail"
+
+
+        time.sleep(5)
